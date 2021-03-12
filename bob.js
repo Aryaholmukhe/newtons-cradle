78e34f88d46058468d92bc100f29dde583664ef4
Aryaholmukhe <68239007+Aryaholmukhe@users.noreply.github.com>
2021-03-11 19:35:06 -0500

    fixed version

--- bob.js
+++ bob.js
@@ -2,20 +2,20 @@ class Bob{
     constructor(x, y){
          var options={
              "isStatic":false,
-             "restitution":0.3,
-             "friction":0.5,
-             "density":1.2
+             "restitution":1.1,
+             "density":0.01
          }
-         this.body = Bodies.circle(x, y,1, options)
-         this.body.radius=1;
+         this.body = Bodies.circle(x, y, 40, options)
+         this.body.radius=79;
          World.add(world, this.body)
     }
 
     display(){
         var pos =this.body.position;
+        fill("#865858")
         push();
         translate(pos.x, pos.y);
-        ellipse(0,0,this.radius)
+        ellipse(0,0,this.body.radius)
         pop();
     }
 
